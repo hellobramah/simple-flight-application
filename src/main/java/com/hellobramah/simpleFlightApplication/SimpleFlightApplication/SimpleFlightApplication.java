45f4215c0e8d4dfbbb7761e307be877bf15f2f59
Alex Bramah-Lawani <hellobramah@gmail.com>
2019-04-21 05:06:50 +0000

    Initial commit

---
+++ src/main/java/com/hellobramah/simpleFlightApplication/SimpleFlightApplication/SimpleFlightApplication.java
@@ -0,0 +1,13 @@
+package com.hellobramah.simpleFlightApplication.SimpleFlightApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFlightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellobramah/simpleFlightApplication/SimpleFlightApplication/SimpleFlightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hellobramah.simpleFlightApplication.SimpleFlightApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleFlightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
